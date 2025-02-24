66d0f3ac0f74be7ea95b03a8481d286771fe074f
akash waghmare <133653971+Akashwaghmare0507@users.noreply.github.com>
2025-02-24 21:21:10 +0530

    add the react project

---
+++ day-01/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+ const App = () => {
+  return  <h1 className="underline font-bold text-blue-900 text-center">welcome to 15 days live reactjs project series.</h1>;
+   
+ <h1>akash</h1>
+};
+
+export default App;
+
