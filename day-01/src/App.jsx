0d8bc3cfe536f138a7449a6aaa01af93b58abb52
akash waghmare <133653971+Akashwaghmare0507@users.noreply.github.com>
2025-02-24 21:52:56 +0530

    complete the counter app

--- day-01/src/App.jsx
+++ day-01/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useState}from 'react';
 
  const App = () => {
-  return  <h1 className="underline font-bold text-blue-900 text-center">welcome to 15 days live reactjs project series.</h1>;
+
+  const [counter, setCounter] = useState(0);
+  return  (
+    <>
+  <h1 className="text-3xl  font-bold text-blue-900 ">{counter}
+    </h1>
+    <button className="border-1 border-slate-700 rounded sm"
+     onClick={()=> setCounter(counter + 1)}
+     >Click me</button>
+     <button className="ml-2 border-1 border-slate-700 rounded sm" 
+     onClick={() => setCounter(0)}
+     >
+      Reset
+      </button>
+
+    </>
+  );
    
  <h1>akash</h1>
 };
